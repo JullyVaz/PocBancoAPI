77beb5c30a38cfd5c92b90448808d02e2a8e832a
Juliane.Vaz <julianeraquel@hotmail.com>
2023-07-14 21:56:09 -0300

    Finished AutoMapper Implementation

---
+++ PocBancoAPI.Enums/AccountTypeEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace PocBancoAPI.Enums
+{
+    public enum AccountTypeEnum
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ PocBancoAPI.Shared/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PocBancoAPI.DTOs;
+using PocBancoAPI.Entities;
+using PocBancoAPI.ViewModels;
+
+namespace PocBancoAPI.Shared.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AccountViewModel, AccountDTO>()
+                .ReverseMap();
+
+            CreateMap<AccountDTO, Account>()
+                .ReverseMap();
+        }
+    }
+}
