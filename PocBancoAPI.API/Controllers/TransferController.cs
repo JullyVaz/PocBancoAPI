96bc7987ccfbf4746ad99de62ec981f9a10642fb
Juliane.Vaz <julianeraquel@hotmail.com>
2023-08-18 11:47:59 -0300

    Updated TransferController

--- PocBancoAPI.API/Controllers/TransferController.cs
+++ PocBancoAPI.API/Controllers/TransferController.cs
@@ -3,6 +3,7 @@ using PocBancoAPI.Services;
 using PocBancoAPI.Services.Interfaces;
 using PocBancoAPI.ViewModels;
 using PocBancoAPI.ViewModels.Filters;
+using System.Net;
 
 namespace PocBancoAPI.API.Controllers
 {
@@ -18,20 +19,54 @@ namespace PocBancoAPI.API.Controllers
 
         [HttpPost]
         [Route("insert")]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Insert([FromBody] TransferViewModel transferViewModel)
         {
-            return Ok(await _transferService.Insert(transferViewModel));
+            ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = await _transferService.InsertAsync(transferViewModel);
+            return StatusCode((int)serviceResponseViewModel.StatusCode, serviceResponseViewModel);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromBody] TransferViewModel transferViewModel)
+        {
+            if (transferViewModel.IdTransfer <= 0)
+            {
+                ServiceResponseViewModel<TransferViewModel> errorResponse = new ServiceResponseViewModel<TransferViewModel>
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = "O campo 'IdTransfer' é obrigatório para a atualização.",
+                    HasData = false
+                };
+
+                return BadRequest(errorResponse);
+            }
+
+            ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = await _transferService.UpdateAsync(transferViewModel);
+            return StatusCode((int)serviceResponseViewModel.StatusCode, serviceResponseViewModel);
         }
 
         [HttpGet]
-        [Route("get-all")]
-        public async Task<IActionResult> GetAll([FromQuery] TransferFilter transferFilter)
+        [Route("get/{id}")]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponseViewModel<TransferViewModel>), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(int id)
         {
-            return Ok(await _transferService.GetAll(transferFilter));
+            ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = await _transferService.GetByIdAsync(id);
+            return StatusCode((int)serviceResponseViewModel.StatusCode, serviceResponseViewModel);
         }
-
-
     }
 }
+
+
+
+
+
+
 
 
