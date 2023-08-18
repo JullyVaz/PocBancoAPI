09f81f4c5fb6154813a487c443ddb94c00c55be8
Juliane.Vaz <julianeraquel@hotmail.com>
2023-08-18 10:53:27 -0300

    Updated TransferService and TransferBusiness

--- PocBancoAPI.Business/Interfaces/ITransferBusiness.cs
+++ PocBancoAPI.Business/Interfaces/ITransferBusiness.cs
@@ -13,6 +13,3 @@ namespace PocBancoAPI.Business.Interfaces
     }
 }
 
-
-
-
--- PocBancoAPI.Services/TransferService.cs
+++ PocBancoAPI.Services/TransferService.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using PocBancoAPI.Business.Interfaces;
+using PocBancoAPI.Data.UnitOfWork;
 using PocBancoAPI.DTOs;
 using PocBancoAPI.Services.Interfaces;
 using PocBancoAPI.ViewModels;
 using PocBancoAPI.ViewModels.Filters;
+using System.Net;
 
 namespace PocBancoAPI.Services;
 
@@ -11,40 +13,82 @@ public class TransferService : ITransferService
 {
     private readonly ITransferBusiness _transferBusiness;
     private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public TransferService(ITransferBusiness transferBusiness, IMapper mapper)
+    public TransferService(ITransferBusiness transferBusiness, IMapper mapper, IUnitOfWork unitOfWork)
     {
         _transferBusiness = transferBusiness;
         _mapper = mapper;
+        _unitOfWork = unitOfWork;
     }
 
-    public async Task<ServiceResponseViewModel<List<TransferViewModel>>> GetAll(TransferFilter transferFilter)
+    public async Task<ServiceResponseViewModel<List<TransferViewModel>>> GetAllAsync(TransferFilter transferFilter)
     {
         ServiceResponseViewModel<List<TransferViewModel>> serviceResponseViewModel = new ServiceResponseViewModel<List<TransferViewModel>>();
         try
         {
-        
         }
         catch (Exception ex)
         {
-            serviceResponseViewModel.Message = ex.GetBaseException().Message;
-            throw;
+            serviceResponseViewModel = new ServiceResponseViewModel<List<TransferViewModel>>(ex);
+            await _unitOfWork.RollBackAsync();
         }
         return serviceResponseViewModel;
     }
 
-    public async Task<ServiceResponseViewModel<TransferViewModel>> Insert(TransferViewModel transferViewModel)
+    public async Task<ServiceResponseViewModel<TransferViewModel>> GetByIdAsync(int Id)
+    {
+        ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>();
+        try
+        {
+            TransferDTO transferDTO = await _transferBusiness.GetByIdAsync(Id);
+            TransferViewModel transferViewModel = _mapper.Map<TransferViewModel>(transferDTO);
+            serviceResponseViewModel.Data = transferViewModel;
+        }
+        catch (Exception ex)
+        {
+            serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>(ex);
+            await _unitOfWork.RollBackAsync();
+        }
+        return serviceResponseViewModel;
+    }
+
+    public async Task<ServiceResponseViewModel<TransferViewModel>> InsertAsync(TransferViewModel transferViewModel)
     {
         ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>();
         try
         {
             TransferDTO transferDTO = _mapper.Map<TransferDTO>(transferViewModel);
+            transferViewModel.IdTransfer = await _transferBusiness.InsertAsync(transferDTO);
+            serviceResponseViewModel.StatusCode = HttpStatusCode.Created;
+            serviceResponseViewModel.Data = transferViewModel;
+            await _unitOfWork.CommitAsync();
         }
         catch (Exception ex)
         {
-            serviceResponseViewModel.Message = ex.GetBaseException().Message;
-            throw;
+            serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>(ex);
+            await _unitOfWork.RollBackAsync();
+        }
+        return serviceResponseViewModel;
+    }
+
+    public async Task<ServiceResponseViewModel<TransferViewModel>> UpdateAsync(TransferViewModel transferViewModel)
+    {
+        ServiceResponseViewModel<TransferViewModel> serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>();
+        try
+        {
+            TransferDTO transferDTO = _mapper.Map<TransferDTO>(transferViewModel);
+            transferDTO = await _transferBusiness.UpdateAsync(transferDTO);
+            serviceResponseViewModel.StatusCode = HttpStatusCode.OK;
+            serviceResponseViewModel.Data = _mapper.Map<TransferViewModel>(transferDTO);
+            await _unitOfWork.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            serviceResponseViewModel = new ServiceResponseViewModel<TransferViewModel>(ex);
+            await _unitOfWork.RollBackAsync();
         }
         return serviceResponseViewModel;
     }
 }
+
